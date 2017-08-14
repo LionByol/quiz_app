3cefb1d4ee76992f8222a3a8f33a35bc69a79182
LionByol <lionbyol@gmail.com>
2017-08-15 00:34:52 +0200

    Update LoginActivity.java

--- app/src/main/java/com/silver/sponsor/LoginActivity.java
+++ app/src/main/java/com/silver/sponsor/LoginActivity.java
@@ -190,7 +190,6 @@ public class LoginActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
     {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_login, menu);
         return true;
     }
